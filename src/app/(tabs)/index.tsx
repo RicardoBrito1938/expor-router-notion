8b774ad021a1bce178b36e656779027b510ce541
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-11-10 17:05:16 +0000

    chore: test design sf

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,12 +2,24 @@ import { Header } from "@/components/header";
 import { PageList } from "@/components/page-list";
 import { RecentList } from "@/components/recent-list";
 import { DATA } from "@/utils/data";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Alert, View } from "react-native";
+import { Button } from "@sf-digital-ui/react-native";
 
 export default function Index() {
+  const createTwoButtonAlert = () =>
+    Alert.alert("Alert Title", "My Alert Msg", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
   return (
     <View style={{ flex: 1, paddingTop: 32 }}>
       <Header />
+      <Button title="Click me" onPress={createTwoButtonAlert} />
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
